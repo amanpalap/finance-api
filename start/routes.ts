2090dec803bd19c77c7542fe8958bf706cc74fd5
amanpalap <amanpalap210@gmail.com>
2025-02-10 18:52:16 +0530

    added: app

---
+++ start/routes.ts
@@ -0,0 +1,27 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router';
+import { middleware } from './kernel.js';
+const AuthController = () => import('#controllers/auth_controller');
+
+// Default route
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  };
+});
+
+// Grouped API routes with prefix 'api/v1'
+router.group(() => {
+  router.post('/signup', [AuthController, 'signup']);
+  router.post('/login', [AuthController, 'login']);
+  router.get('/me', [AuthController, 'me']).use(middleware.auth({ guards: ['api'] }));
+}).prefix('api/v1');
+
